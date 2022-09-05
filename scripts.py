60800cc30560b511b1a8f56d91e7a2fe976062c3
Rafael Cunha <rafaelcunha2013@gmail.com>
2022-09-05 10:18:51 +0200

    scripts.py with classes Flatmate, Bill, and PdfReport created

---
+++ scripts.py
@@ -0,0 +1,30 @@
+class Flatmate:
+    """
+    Saves information as name, days in the house amount to pay in the bill of a flatmate in the house.
+    """
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        return (self.days_in_house/30) * bill
+
+
+class Bill:
+    """
+    Has the amount and period of a bill
+    """
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+
+
+class PdfReport:
+    """
+    Generates a PDF report stating the names of the flatmates, the period, and how much each of them had to pay.
+    """
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
